4e002da7f90cbb509ab0de585dccabed90a8a1f2
Vanessa Guerrero <vamarilug@gmail.com>
2020-06-26 13:04:26 -0700

    Successfully fail first correct test

--- __tests__/supregalactic-tests.js
+++ __tests__/supregalactic-tests.js
@@ -1,15 +1,17 @@
-import { Calculate } from '../src/supergalactic-age-calculator.js';
+import { Age } from '../src/supergalactic-age-calculator.js';
 
 describe ('Age', () => {
-  test('should display age in planetary years', () => {
-    let planetAge = new Calculate(30);
-    expect(planetAge.mercury).toEqual(7.2);
-    expect(planetAge.venus).toEqual(18.6);
-    expect(planetAge.mars).toEqual(56.4);
-    expect(planetAge.jupiter).toEqual(11.86);
+  test('should calculate age in planetary years', () => {
+    let planetAge = new Age(30);
+    expect(planteAge.checkAge()).toEqual(7.2);
   })
 })
 
  
 
-  // expect(planteAge.checkAge()).toEqual(7.2);
+  
+
+   // expect(planetAge.mercury).checkAge().toEqual();
+    // expect(planetAge.venus).toEqual(18.6);
+    // expect(planetAge.mars).toEqual(56.4);
+    // expect(planetAge.jupiter).toEqual(11.86);
--- src/supergalactic-age-calculator.js
+++ src/supergalactic-age-calculator.js
@@ -1,13 +1,13 @@
-export class Calculate {
+export class Age {
   // constructor (mercury, venus, mars, jupiter) {
   //   this.mercury = mercury;
   //   this.venus = venus;
   //   this.mars = mars;
   //   this.jupiter = jupiter;
-  //}
+  // }
 }
 
-//checkAge() {
+checkAge() {
 
   
-//}
+}
