ccd8a537b111d51c4d16042d5f3d385864263767
Vanessa Guerrero <vamarilug@gmail.com>
2020-06-26 11:51:05 -0700

    Succcessfully fail test to display age in planetary years

---
+++ __tests__/supregalactic-tests.js
@@ -0,0 +1,13 @@
+import { Calculate } from '../src/supergalactic-age-calculator.js';
+
+describe ('Age', () => {
+  test('should display age in planetary years', () => {
+    let planetAge = new Calculate(30);
+    expect(planteAge.checkAge()).toEqual(7.2);
+  })
+})
+
+ // expect(planetAge.mercury).toEqual(7.2);
+    // expect(planetAge.venus).toEqual(18.6);
+    // expect(planetAge.mars).toEqual(56.4);
+    // expect(planetAge.jupiter).toEqual(11.86);
---
+++ src/supergalactic-age-calculator.js
@@ -0,0 +1,12 @@
+export class Calculate {
+  // constructor (mercury, venus, mars, jupiter) {
+  //   this.mercury = mercury;
+  //   this.venus = venus;
+  //   this.mars = mars;
+  //   this.jupiter = jupiter;
+  //}
+}
+
+checkAge() {
+  
+}
